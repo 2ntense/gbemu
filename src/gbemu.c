5d4f6168948a3538f655a761a1ba66eeecab9812
2ntense <stefanyip@hotmail.com>
2020-04-15 18:15:09 +0200

    initiate post pwr seq at boot

--- src/gbemu.c
+++ src/gbemu.c
@@ -1,11 +1,13 @@
 #include "system.h"
 #include "instructions.h"
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
     gb_t *gb = init_gb();
     load_rom(gb);
+    post_power_seq(gb);
 
 #ifdef DEBUG
     printf("Start loop\n");
@@ -13,5 +15,9 @@ int main(int argc, char **argv)
     while (1)
     {
         emulate_cycle(gb);
+#ifdef DEBUG
+#define DELAY_MS 1000
+        usleep(200 * DELAY_MS);
+#endif
     }
 }
